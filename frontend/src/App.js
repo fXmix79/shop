fa2171db9b89b1863ade90349d548d508647b038
Fabijan Mošnja <fmosnja@gmail.com>
2024-06-23 22:49:54 +0200

    Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
-  return(
-    <h1>Welcome to Shop</h1>
-  )
-}
-export default App
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
